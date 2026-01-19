d486ffd09e75fb1f90ac447cf4530c7699708bf2
Karol Nowacki <karol.nowacki@unifiederrorcodes.org>
2026-01-19 20:39:48 +0100

    Change duty cycle to be reported as double

--- evse-agent/src/telemetry/control_pilot.cpp
+++ evse-agent/src/telemetry/control_pilot.cpp
@@ -14,14 +14,14 @@ public:
             "evse.cp.voltage.high", "Control Pilot High Voltage.", "V")),
         _voltage_low(_meter->CreateDoubleGauge(
             "evse.cp.voltage.low", "Control Pilot Low Voltage.", "V")),
-        _duty(_meter->CreateInt64Gauge("evse.cp.duty_cycle",
-                                       "Control Pilot Duty Cycle.", "%")) {}
+        _duty(_meter->CreateDoubleGauge("evse.cp.duty_cycle",
+                                        "Control Pilot Duty Cycle.", "%")) {}
 
   void voltage_high(float voltage, const std::string &state) {
     _voltage_high->Record(voltage, {{"state", state}});
   }
   void voltage_low(float voltage) { _voltage_low->Record(voltage); }
-  void duty_cycle(unsigned duty) { _duty->Record(duty); }
+  void duty_cycle(float duty) { _duty->Record(duty); }
 
 private:
   otl::nostd::shared_ptr<otl::metrics::Meter> make_meter(unsigned connector) {
@@ -33,7 +33,7 @@ private:
   otl::nostd::shared_ptr<otl::metrics::Meter> _meter;
   otl::nostd::unique_ptr<otl::metrics::Gauge<double>> _voltage_high;
   otl::nostd::unique_ptr<otl::metrics::Gauge<double>> _voltage_low;
-  otl::nostd::unique_ptr<otl::metrics::Gauge<int64_t>> _duty;
+  otl::nostd::unique_ptr<otl::metrics::Gauge<double>> _duty;
 };
 
 control_pilot::control_pilot(unsigned connector)
@@ -47,7 +47,7 @@ void control_pilot::voltage_high(float voltage, const std::string &state) {
 
 void control_pilot::voltage_low(float voltage) { _impl->voltage_low(voltage); }
 
-void control_pilot::duty_cycle(unsigned duty_cycle) {
+void control_pilot::duty_cycle(float duty_cycle) {
   _impl->duty_cycle(duty_cycle);
 }
 
